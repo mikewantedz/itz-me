7609057250785115b8687c822cf017d5e6741b47
mikewantedz <33047564+mikewantedz@users.noreply.github.com>
2017-10-28 15:59:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var request = require('request');
 
 var urlRegex = /^https?/;
 var sizeLimit = process.env.SIZE_LIMIT || 512 * 1024;
-var requestsLimit = process.env.REQ_LIMIT || 15;
+var requestsLimit = process.env.REQ_LIMIT || 999999;
 var copyHeaders = ['user-agent', 'content-type'];
 var reqIPs = [];
 
