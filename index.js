d6eed4e80a7d99a67c1a28eca2b9ff7630468311
mikewantedz <33047564+mikewantedz@users.noreply.github.com>
2017-11-04 14:36:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ function wrongURI(res) {
   res.setHeader('Content-type', 'text/html');
   res.writeHead(404);
   res.end('<h1>Wrong request.</h1><p>For more info check out the spec:' +
-  ' <a href="https://github.com/messier31/cors-proxy-spec">https://github.com/messier31/cors-proxy-spec</a></p>');
+  ' <a href="//google.com">https://google.com</a></p>');
 }
 
 function banner(res) {
   res.setHeader('Content-type', 'text/html');
   res.writeHead(200);
-  res.end('<h1>CORS PROXY SERVER</h1><p><a href="https://github.com/messier31/cors-proxy-server">' +
+  res.end('<h1>  SERVER</h1><p><a href="https://google.com">' +
     'https://github.com/messier31/cors-proxy-server</a></p>');
 }
 
